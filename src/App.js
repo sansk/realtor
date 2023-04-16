ca3f73a447fd6453073f7b65975f6c5d152d93ca
sansk <iamsangyk@gmail.com>
2023-04-16 07:54:05 +0530

    Created Pages ad Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-5xl font-bold underline bg-gray-50">Hello Realtor World!</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
